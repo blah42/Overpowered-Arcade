44f3b175d2d6b68c6740764e77b7d629fcbe210a
mmann6152 <88596909+mmann6152@users.noreply.github.com>
2025-07-18 13:57:44 -0400

    scripts folder
    for the scripts

---
+++ new-game-project/Scripts/ArcadeMachine.gd
@@ -0,0 +1,6 @@
+extends Node
+
+@export var suckRate = 1
+@export var multi = false
+@export var type = 0
+@export var stats = [1, 1, 1, 1]
---
+++ new-game-project/Scripts/Enemies.gd
@@ -0,0 +1,5 @@
+extends Node
+
+@export var stats = [1, 1, 1]
+@export var type = 0
+@export var power = 1
