9a1b8a0068241c820114e21de15785a55d5bb84c
blah42 <stevencoulter1808@yahoo.com>
2025-07-19 09:13:39 -0400

    modified:   new-game-project/Scenes/GenericEnemies.tscn 	modified:   new-game-project/Scenes/Main.tscn 	modified:   new-game-project/Scenes/testing_map.tscn 	modified:   new-game-project/Scripts/Main.gd 	modified:   new-game-project/Scripts/Souls.gd 	new file:   new-game-project/Scripts/button.gd 	new file:   new-game-project/Scripts/button.gd.uid 	new file:   new-game-project/Scripts/button_floor_square_2.gd 	new file:   new-game-project/Scripts/button_floor_square_2.gd.uid 	modified:   new-game-project/Scripts/buy_bar.gd 	new file:   new-game-project/Scripts/collision_shape_3d.gd 	new file:   new-game-project/Scripts/collision_shape_3d.gd.uid 	modified:   new-game-project/assets/converted_assets/button_floor_square_2.tscn 	renamed:    new-game-project/shaders/tile_select_shader.gdshader -> new-game-project/assets/tile_select_shader.gdshader 	renamed:    new-game-project/shaders/tile_select_shader.gdshader.uid -> new-game-project/assets/tile_select_shader.gdshader.uid 	modified:   new-game-project/shaders/tile_select_shader.tres

--- new-game-project/Scripts/Main.gd
+++ new-game-project/Scripts/Main.gd
@@ -1,4 +1,10 @@
 extends Node
+var arrow = load("res://assets/kenney_cursor-pixel-pack/Tiles/tile_0046.png")
+var hand = load("res://assets/kenney_cursor-pixel-pack/Tiles/tile_0137.png")
+var click_build = load("res://assets/kenney_ui-pack/Sounds/click-b.ogg")
+var click_bar= load("res://assets/kenney_ui-pack/Sounds/click-a.ogg")
+var air_hockey = preload("res://Scenes/AirHockey.tscn")
+@export var selected_object = null;
 signal make_menu_bar_visible;
 signal make_menu_bar_invisible;
 signal souls_changed;
@@ -7,11 +13,15 @@ signal score_changed;
 
 func _on_build_tile_button_pressed():
 	#make click sound
+	#AudioStreamPlayer.
 	make_menu_bar_visible.emit();
 	pass # Replace with function body.
 func _on_build_success_airhockey():
 	#make click sound
-	
+	var new_air_hockey = air_hockey.instantiate();
+	new_air_hockey.position = selected_object.position;
+	print(selected_object.position)
+	add_child(new_air_hockey)
 	make_menu_bar_invisible.emit();
 	pass # Replace with function body.
 func setSouls(value):
@@ -21,23 +31,32 @@ func setSouls(value):
 func setScore(value):
 	$Camera3D/Control/HBoxContainer3/Score.score = value;
 	score_changed.emit();
-func _on_air_hockey_button_pressed():
-	setScore(1000);
-	var cost = 100;
-	if($Camera3D/Control/HBoxContainer/Money.souls <=cost): return;
-	setSouls($Camera3D/Control/HBoxContainer/Money.souls - cost);
-	##
-	## instatiate Air hockey here
-	#3
 	pass # Replace with function body.
 
 	
 const RAY_LENGTH = 100
-
+func _input(event: InputEvent) -> void:
+	if event is InputEventMouseButton and event.is_pressed() and event.button_index == MOUSE_BUTTON_LEFT:
+		var space_state = $Camera3D.get_world_3d().direct_space_state
+		var mousepos = get_viewport().get_mouse_position()
+		var origin = cam.project_ray_origin(mousepos)
+		var end = origin + cam.project_ray_normal(mousepos) * RAY_LENGTH
+		var query = PhysicsRayQueryParameters3D.create(origin, end)
+		query.collide_with_areas = true
+		var colission = space_state.intersect_ray(query)
+		if(colission):
+			var object = colission.collider
+			if(object.name == "Placement_tile"):
+				print("tile_clicked")
+				selected_object = colission;
+				_on_build_tile_button_pressed()
+			else:
+				make_menu_bar_invisible.emit()
+				
+			
 func _physics_process(_delta):
 	var space_state = $Camera3D.get_world_3d().direct_space_state
 	var mousepos = get_viewport().get_mouse_position()
-
 	var origin = cam.project_ray_origin(mousepos)
 	var end = origin + cam.project_ray_normal(mousepos) * RAY_LENGTH
 	var query = PhysicsRayQueryParameters3D.create(origin, end)
@@ -45,10 +64,22 @@ func _physics_process(_delta):
 	var colission = space_state.intersect_ray(query)
 	if(colission):
 		var object = colission.collider
-		if(object.name == "button_floor_square_2"):
-			print("button")
+		if(object.name == "Placement_tile"):
+			Input.set_custom_mouse_cursor(hand)
+		else:
+			Input.set_custom_mouse_cursor(arrow)
+			
+			
 			
 		
 		#if(compstr.beginsWith("floor_alt")):
 		#	print('highlight')
 	pass
+
+
+func _on_air_hockey_button_pressed() -> void:
+	var cost = 100;
+	if($Camera3D/Control/HBoxContainer/Money.souls <=cost): return;
+	setSouls($Camera3D/Control/HBoxContainer/Money.souls - cost);
+	_on_build_success_airhockey()
+	pass # Replace with function body.
