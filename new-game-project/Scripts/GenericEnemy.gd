fbf8b758376fdb2591401997f668be0b508edeae
mmann6152 <88596909+mmann6152@users.noreply.github.com>
2025-07-19 21:38:31 -0400

    better but not great targeting program

--- new-game-project/Scripts/GenericEnemy.gd
+++ new-game-project/Scripts/GenericEnemy.gd
@@ -12,8 +12,9 @@ var soulWorth = 10
 var playTime = 50
 var playing = false
 var totalSouls : String
-#signal soulsAbsorbed
 var arcadeTitle : CharacterBody3D
+var startingIndex = 0
+var tempIdx = 0
 
 func _physics_process(_delta):
 	if not playing:
@@ -28,32 +29,69 @@ func _physics_process(_delta):
 			finishedPlaying()
 		
 	if not acquiring and not playing:
-		for child in detector.get_children():
-			if((not child.taken) and path.position.x<child.position.x and not acquiring and not child == arcadeTitle):
+		var obj = $"RayCast3D".get_collider()
+		if(not obj==null):
+			print("Arcade Detected")
+			if(not obj.taken and not obj == arcadeTitle):
+				print("Targeting Arcade")
 				acquiring = true
-				child.taken = true
-				arcadeTitle = child
-				soulDrain = child.damage
-				#print("Machine Detected")
-				#nextPoint = path.add_point(,,)
-				#print(child.position)
-				characterPath.curve.add_point(child.position,Vector3(0,0,0),Vector3(0,0,0),(characterPath.curve.point_count-1))
-				break
-			else:
-				#print("Ignored")
-				pass
+				obj.taken = true
+				arcadeTitle = obj
+				soulDrain = obj.damage
+				print("Object Position")
+				print(obj.position)
+				print("Position")
+				print(path.position)
+				#var closest = characterPath.curve.get_closest_point()
+				#print("ClosestPoint")
+				#print(closest)
+				tempIdx = 0
+				var closest = 10000
+				for n in range(startingIndex, characterPath.curve.point_count):
+					#print("Looking for point")
+					#print(characterPath.curve.get_point_position(n))
+					var diff = characterPath.curve.get_point_position(n).distance_to(path.position)
+					if closest > diff:
+						closest = diff
+						tempIdx = n
+					
+						#var halfWay = characterPath.curve.sample(tempIdx, 0.5)
+						#if path.position.distance_to()
+				print(tempIdx)		
+				characterPath.curve.add_point(obj.position, Vector3(0,0,0), Vector3(0,0,0), tempIdx+1)
+				print("Full List")
+				for n in characterPath.curve.point_count:
+					print(characterPath.curve.get_point_position(n))
+				startingIndex = tempIdx + 1
+		#var camera3d = $Camera3D
+		#var from = camera3d.project_ray_origin(path.position)
+		#var to = from + camera3d.project_ray_normal(path.position) * 1000
+		#for child in detector.get_children():
+			
+			#if((not child.taken) and path.position.x<child.position.x and not acquiring and not child == arcadeTitle):
+				#acquiring = true
+				#child.taken = true
+				#arcadeTitle = child
+				#soulDrain = child.damage
+				##print("Machine Detected")
+				##nextPoint = path.add_point(,,)
+				##print(child.position)
+				#characterPath.curve.add_point(child.position,Vector3(0,0,0),Vector3(0,0,0),(characterPath.curve.point_count-1))
+				#break
+
 	if acquiring:
-		print("Acquiring")
-		if (characterPath.curve.get_point_position((characterPath.curve.point_count-2)).distance_to(path.position)<0.5):
+		#print("Acquiring")
+		if (characterPath.curve.get_point_position(tempIdx+1).distance_to(path.position)<0.5):
 			acquiring = false
 			playing = true
+			tempIdx = 0
 			$AnimationPlayer.play("playing",-1,5);
 			print("Acquired")
 	#print($GenericEnemies.Transform.position.x)
 
 	if path.progress_ratio==1:
 		queue_free()
-	if health == 0:
+	if health <= 0:
 		$AnimationPlayer.play("death",-1,5);
 		await get_tree().create_timer(.2).timeout
 		finishedPlaying()
@@ -68,6 +106,8 @@ func initialize(startPath:Path3D, chosenPath: PathFollow3D, detection: Node3D, s
 	detector = detection
 	path = chosenPath
 	rotate_y(-PI)
+	#$Camera3D/RayCast3D.set_collision_mask_value(1, false)
+	#$Camera3D/RayCast3D.set_collision_mask_value(2, true)
 
 func finishedPlaying():
 	print("Finished playing")
